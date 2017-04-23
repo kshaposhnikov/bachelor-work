80c96f3e13de8a2e60f37e349c319295716e9e84
kirill <shaposhnikov.ks@gmail.com>
2017-04-23 23:25:11 +0400

    Implement settings page

--- src/main/java/com/shaposhnikov/facerecognizer/recognizer/OpenCVRecognizeContainer.java
+++ src/main/java/com/shaposhnikov/facerecognizer/recognizer/OpenCVRecognizeContainer.java
@@ -1,56 +1,21 @@
 package com.shaposhnikov.facerecognizer.recognizer;
 
-import com.shaposhnikov.facerecognizer.util.ImageConverter;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Kirill on 14.03.2017.
+ * Created by Kirill on 22.04.2017.
  */
-public class OpenCVRecognizeContainer {
+public interface OpenCVRecognizeContainer {
 
-    private List<Mat> imagesToTrain = new ArrayList<>();
-    private List<Integer> labels = new ArrayList<>();
+    void load();
 
-    private final String pathToResult;
+    String getPathToResult();
 
-    public OpenCVRecognizeContainer(String pathToResult) {
-        this.pathToResult = pathToResult;
-    }
+    List<Mat> getImagesToTrain();
 
-    public void load(String pathToSource) throws IOException {
-        load(pathToSource, "");
-    }
+    Mat getLabels();
 
-    private void load(String pathToSource, String parent) throws IOException {
-        File file = new File(pathToSource);
-        for (File item : file.listFiles()) {
-            if (item.isDirectory()) {
-                load(item.getAbsolutePath(), item.getName());
-            } else {
-                labels.add(new Integer(parent));
-                imagesToTrain.add(ImageConverter.bufferedImageToMat(ImageIO.read(item)));
-            }
-        }
-    }
-
-    public List<Mat> getImagesToTrain() {
-        return imagesToTrain;
-    }
-
-    public Mat getLabels() {
-        MatOfInt res = new MatOfInt();
-        res.fromList(labels);
-        return res;
-    }
-
-    public String getPathToResult() {
-        return pathToResult;
-    }
+    boolean isLoaded();
 }
