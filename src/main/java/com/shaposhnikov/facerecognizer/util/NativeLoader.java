a04ad0dfb40091b42516baa6e81fdafadbf6a5b5
kirill <shaposhnikov.ks@gmail.com>
2017-05-05 00:07:52 +0400

    Refactoring for the NativeLoader to looking libs in the all ClassLoaders chain

--- src/main/java/com/shaposhnikov/facerecognizer/util/NativeLoader.java
+++ src/main/java/com/shaposhnikov/facerecognizer/util/NativeLoader.java
@@ -3,12 +3,17 @@
  */
 package com.shaposhnikov.facerecognizer.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.*;
 
 public class NativeLoader {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NativeLoader.class);
 
     private static final String NATIVE_LIB_PATH = "opencv.native.lib.path";
     private static final String NATIVE_LIB_EXTENSION = "opencv.native.lib.extension";
@@ -27,32 +32,8 @@ public class NativeLoader {
     }
 
     public void load(String name) {
-        try {
-            Map<String, Boolean> nativeLibraryStatuses = new HashMap<>();
-
-            ClassLoader parent = this.getClass().getClassLoader().getParent();
-            Field nativeLibraries = ClassLoader.class.getDeclaredField("nativeLibraries");
-            nativeLibraries.setAccessible(true);
-
-            Vector<Object> libraries = (Vector<Object>) nativeLibraries.get(parent);
-            for (Object item : libraries) {
-                Field loadedLibraryName = item.getClass().getDeclaredField("name");
-                loadedLibraryName.setAccessible(true);
-
-                Field loadedLibraryStatus = item.getClass().getDeclaredField("loaded");
-                loadedLibraryStatus.setAccessible(true);
-
-                String loadedLibraryNameValue = (String) loadedLibraryName.get(item);
-                Boolean loadedLibraryStatusValue = loadedLibraryStatus.getBoolean(item);
-
-                loadedLibraryNameValue = loadedLibraryNameValue.substring(
-                        loadedLibraryNameValue.lastIndexOf(File.separatorChar) + 1,
-                        loadedLibraryNameValue.lastIndexOf(".dll"));
-
-                nativeLibraryStatuses.put(loadedLibraryNameValue, loadedLibraryStatusValue);
-            }
-
-            if (!nativeLibraryStatuses.containsKey(name)) {
+        if (!isLibraryLoaded(this.getClass().getClassLoader(), name)) {
+            try {
                 Properties properties = new Properties();
                 properties.load(getClass().getResourceAsStream("config.properties"));
 
@@ -60,14 +41,53 @@ public class NativeLoader {
                         name +
                         properties.getProperty(buildExtenstionProperty())
                 );
+
+                LOGGER.debug("Library {} loaded successfully", name);
+            } catch (IOException e) {
+                LOGGER.error("Couldn't open static \"config.properties\"", e);
+                throw new RuntimeException("Couldn't open static \"config.properties\"", e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Couldn't open static \"config.properties\"", e);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
         }
+    }
+
+    private boolean isLibraryLoaded(ClassLoader currentLoader, String name) {
+        try {
+            if (currentLoader != null) {
+                Map<String, Boolean> nativeLibraryStatuses = new HashMap<>();
+                Field nativeLibraries = ClassLoader.class.getDeclaredField("nativeLibraries");
+                nativeLibraries.setAccessible(true);
+
+                Vector<Object> libraries = (Vector<Object>) nativeLibraries.get(currentLoader);
+                for (Object item : libraries) {
+                    Field loadedLibraryName = item.getClass().getDeclaredField("name");
+                    loadedLibraryName.setAccessible(true);
+
+                    Field loadedLibraryStatus = item.getClass().getDeclaredField("loaded");
+                    loadedLibraryStatus.setAccessible(true);
+
+                    String loadedLibraryNameValue = (String) loadedLibraryName.get(item);
+                    Boolean loadedLibraryStatusValue = loadedLibraryStatus.getBoolean(item);
+
+                    loadedLibraryNameValue = loadedLibraryNameValue.substring(
+                            loadedLibraryNameValue.lastIndexOf(File.separatorChar) + 1,
+                            loadedLibraryNameValue.lastIndexOf(".dll"));
+
+                    nativeLibraryStatuses.put(loadedLibraryNameValue, loadedLibraryStatusValue);
+                }
+
+                if (nativeLibraryStatuses.containsKey(name)) {
+                    LOGGER.debug("Library {} already loaded with {}", name, currentLoader);
+                    return true;
+                } else {
+                    return isLibraryLoaded(currentLoader.getParent(), name);
+                }
+            }
+            return false;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.error("Couldn't get access to the field", e);
+            throw new RuntimeException("Couldn't get access to the field", e);
+        }
+
     }
 
     private String buildPathProperty() {
